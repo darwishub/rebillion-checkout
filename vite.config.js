aae58db2c5b9b56b7d52b70656c87019c6b2a265
sejodoh <teamsejodoh@gmail.com>
2023-03-23 10:19:08 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 import { resolve } from 'path';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
-
+const pathSrc = path.resolve(__dirname, "./src");
 
 export default defineConfig({
   plugins: [
@@ -12,7 +13,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-        "@": resolve(__dirname, "./src"),
+        "@": path.resolve(__dirname, "./src"),
     }
-},
+  },
 })
